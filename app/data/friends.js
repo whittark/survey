609f108c2d938457c795f6786273e276c90cdac6
Whitney Tarkington <wtarkington@athenahealth.com>
2020-03-17 00:24:16 -0700

    adding survey files

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friendMatch = [
+  {
+    name: "Upgrade and activation with EPCS and PDMP",
+    photo: "https://www.phoca.cz/images/projects/phoca-download-r.svg ",
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"] 
+  }, {
+    name: "Upgrade and activation with EPCS",
+    photo: "https://www.phoca.cz/images/projects/phoca-download-r.svg ",
+    scores: ["2", "2", "2", "2", "2", "2", "2", "2", "2", "2"]
+  }, {
+    name: "Upgrade and activation with EPCS and PDMP migration",
+    photo: "https://www.phoca.cz/images/projects/phoca-download-r.svg ",
+    scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+  }, {
+    name: "Upgrade and activation with consultation",
+    photo: "https://www.phoca.cz/images/projects/phoca-download-r.svg ",
+    scores: ["4", "4", "4", "4", "4", "4", "4", "4", "4", "4"]
+  },{
+    name: "Upgrade and activation for non-controlled substances",
+    photo: "https://www.phoca.cz/images/projects/phoca-download-r.svg ",
+    scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+  },
+
+];
+
+
+
+module.exports = friendMatch;
